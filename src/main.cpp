d4612dd89fbc67d08c6f700e3908b8c3df7cc303
BerrocalG <guadalupe.berrocal@usil.pe>
2025-05-27 14:00:36 -0500

    codigo emisor

--- src/main.cpp
+++ src/main.cpp
@@ -40,13 +40,57 @@ int main(void) {
 }
 */
 
+//emisor
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+void config_USART(void) {
+    
+    UCSR0B = (1 << TXEN0); 
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); 
+    UBRR0 = 103;
+}
+
+void senal(char c) {
+    while (!(UCSR0A & (1 << UDRE0)));
+    UDR0 = c;
+}
+
+int main(void) {
+    DDRB &= ~0x03;
+    PORTB |= 0x03;  
+
+    config_USART();
+
+    while (1) {
+        if (!(PINB & 0x01)) { 
+            _delay_ms(100);
+            if (!(PINB & 0x01)) {
+                senal('1'); 
+            }
+        }
+
+        if (!(PINB & 0x02)) {
+            _delay_ms(100);
+            if (!(PINB & 0x02)) {
+                senal('0');
+            }
+        }
+    }
+}
+
+
+
+/*
 // receptor
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-volatile char señal = 0;
+volatile char senal = 0;
 
 void config_USART(void) {
     UCSR0B = (1 << RXEN0) | (1 << RXCIE0); 
@@ -55,7 +99,7 @@ void config_USART(void) {
 }
 
 ISR(USART_RX_vect) {
-    UDR0 = señal;
+    UDR0 = senal;
 }
 
 int main(void) {
@@ -66,12 +110,13 @@ int main(void) {
     sei();
 
     while (1) {
-        if (señal == '1') {
+        if (senal == '1') {
             PORTD |= 0xc0;  
-            señal = 0;
-        } else if (señal == '0') {
+            senal = 0;
+        } else if (senal == '0') {
             PORTD &= ~0x80; 
-            señal = 0;
+            senal = 0;
         }
     }
 }
+*/
