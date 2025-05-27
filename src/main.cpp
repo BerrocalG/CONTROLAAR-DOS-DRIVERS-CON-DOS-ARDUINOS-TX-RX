c7ff9d4e44160702bd44ccf7a9796166d8b46e8f
BerrocalG <guadalupe.berrocal@usil.pe>
2025-05-27 13:50:01 -0500

    receptor base

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+
+/*
 #define F_CPU 16000000UL  
 #include <avr/io.h>  
 #include <util/delay.h>
@@ -34,5 +36,42 @@ int main(void) {
     sei();  
 
     while (1) {
+    }
+}
+*/
+
+// receptor
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+volatile char señal = 0;
+
+void config_USART(void) {
+    UCSR0B = (1 << RXEN0) | (1 << RXCIE0); 
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); 
+    UBRR0 = 103; 
+}
+
+ISR(USART_RX_vect) {
+    UDR0 = señal;
+}
+
+int main(void) {
+    DDRD |= 0xc0; // Motor conectado a PD7
+    PORTD &= ~0xc0; // Apagado al inicio
+
+    config_USART();
+    sei();
+
+    while (1) {
+        if (señal == '1') {
+            PORTD |= 0xc0;  
+            señal = 0;
+        } else if (señal == '0') {
+            PORTD &= ~0x80; 
+            señal = 0;
+        }
     }
 }
